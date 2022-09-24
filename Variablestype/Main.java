bf168ce16a60b287fd9952a1c41247069a997957
MehmetGurbuz0202 <gurbuzmehmet0202@gmail.com>
2022-09-24 12:29:53 +0300

    Odev1
    first week lessson

---
+++ Variablestype/Main.java
@@ -0,0 +1,13 @@
+package Variablestype;
+
+public class Main {
+
+	public static void main(String[] args) {
+		double sayi=12.5;
+		int rakan=15;
+		char karakter='A';
+		boolean dogruMu=false;
+
+	}
+
+}
---
+++ sayiBulma/Main.java
@@ -0,0 +1,24 @@
+package sayiBulma;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int[] sayilar= {0,1,2,3,4,5};
+		int aranacak=5;
+		boolean varMi=false;
+		for(int sayi:sayilar) {
+			if(sayi==aranacak) {
+				varMi=true;
+				break;
+			}
+		}
+		
+		if(varMi) {
+			System.out.println("Sayi mevcuttur.");
+		}
+		else {
+			System.out.println("Sayi mevcut değildir.");
+		}
+	}
+
+}
